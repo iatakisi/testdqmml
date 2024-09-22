882cd99bcda74000acff3179671148aee039ded8
iatakisi <8875661+iatakisi@users.noreply.github.com>
2024-09-22 16:23:52 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,4 +7,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8001)
+    app.run(host='0.0.0.0')
